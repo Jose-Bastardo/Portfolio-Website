a0e5c1084de0655662e9000aa55172022440a8ac
MajorZap <josebastardo123@gmail.com>
2021-05-19 21:19:56 -0400

    Deployment Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
